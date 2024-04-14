b00c43cbce2cb546f9440df46dcc25a3854da72a
parmsam <parmartsam@gmail.com>
2024-04-14 12:42:48 -0400

    Stop tracking target directory

--- guessing_game/target/debug/deps/guessing_game-c85aae68195b6c81.d
+++
@@ -1,5 +0,0 @@
-/Users/samparmar/01-projects/book-work/rust/rust-lang-book/guessing_game/target/debug/deps/guessing_game-c85aae68195b6c81: src/main.rs
-
-/Users/samparmar/01-projects/book-work/rust/rust-lang-book/guessing_game/target/debug/deps/guessing_game-c85aae68195b6c81.d: src/main.rs
-
-src/main.rs:
--- guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/Users/samparmar/01-projects/book-work/rust/rust-lang/guessing_game/target/debug/guessing_game: /Users/samparmar/01-projects/book-work/rust/rust-lang/guessing_game/src/main.rs
