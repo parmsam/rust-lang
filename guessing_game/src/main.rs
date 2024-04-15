1e7b23f1d9c8389ba496c1b884d6060a31ab3993
parmsam <parmartsam@gmail.com>
2024-04-14 20:22:20 -0400

    add exit command to guessing_game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -5,9 +5,10 @@ use rand::Rng;
 fn main() {
     // Inform user about the game
     println!("Guess the number!");
+    // Mention how to quit the game on same line as previous print
+    println!("Type 'quit' or 'exit' to quit the game.");
     // Generate a random number between 1 and 100
     let secret_number = rand::thread_rng().gen_range(1..=100);
-
     loop {
         // Ask for user's guess
         println!("Please input your guess.");
@@ -20,9 +21,18 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
+        // Quit on "quit" or "exit" commands otherwise continue
         let guess: u32 = match guess.trim().parse() {
                 Ok(num) => num,
-                Err(_) => continue,
+                Err(_) => {
+                    if guess.trim() == "quit" || guess.trim() == "exit" {
+                        println!("Quitting game...");
+                        break;
+                    } else {
+                        println!("Please enter a number!");
+                        continue;
+                    }
+                }
         };
 
         // Print user's guess (note: this line has a mistake)
